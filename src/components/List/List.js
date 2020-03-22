e5c9ea170838b2784893a5343e22e1850fe40675
System Administrator <root@adm-e3s-MacBook-Pro.local>
2020-03-22 13:41:49 +0100

    added columns & changed img to not be static

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js'
+import PropTypes from 'prop-types';
+import Column from '../Column/Column';
 
 class List extends React.Component {
+
+    static propTypes = {
+        title: PropTypes.node.isRequired,
+        titleColumn: PropTypes.node,
+        children: PropTypes.node,
+      }
+    static defaultProps = {
+        children: <p>I can do all the things!!!</p>,
+    }
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title} image={this.props.image}/>
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
+        <div className={styles.columns}>
+            <Column titleColumn={'Animals'}/>
+            <Column titleColumn={'Plants'}/>
+            <Column titleColumn={'Minerals'}/>
+        </div>
       </section>
     )
   }
